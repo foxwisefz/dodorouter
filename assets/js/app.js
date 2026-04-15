0032e2d4652fdbb1ab3d8b40151d119c563f8451
Gezim Hoxha <hgezim@gmail.com>
2026-04-16 01:01:50 +0400

    remove glass parallax effect in favor of simpler CSS approach

--- assets/js/app.js
+++ assets/js/app.js
@@ -26,14 +26,13 @@ import {hooks as colocatedHooks} from "phoenix-colocated/dodo_router"
 import {RequestFlowAnimation} from "./request_flow_animation"
 import {LogEntryAnimations} from "./log_entry_animations"
 import {PulseRing} from "./pulse_ring"
-import {GlassParallax} from "./glass_parallax"
 import topbar from "../vendor/topbar"
 
 const csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 const liveSocket = new LiveSocket("/live", Socket, {
   longPollFallbackMs: 2500,
   params: {_csrf_token: csrfToken},
-  hooks: {...colocatedHooks, RequestFlowAnimation, LogEntryAnimations, PulseRing, GlassParallax},
+  hooks: {...colocatedHooks, RequestFlowAnimation, LogEntryAnimations, PulseRing},
 })
 
 // Show progress bar on live navigation and form submits
