f75b5cb2902257142f3c8caea4e00f907f9a712f
Gezim Hoxha <hgezim@gmail.com>
2026-05-30 13:56:03 +0400

    cause build

--- lib/dodo_router/upgrade.ex
+++ lib/dodo_router/upgrade.ex
@@ -9,6 +9,8 @@ defmodule DodoRouter.Upgrade do
   def install(version) when is_binary(version) do
     :application.start(:sasl)
 
+    # it's night time again
+
     root = release_root()
     tarball = Path.join([root, "releases", "#{version}.tar.gz"])
 
