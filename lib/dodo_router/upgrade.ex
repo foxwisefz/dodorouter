3cb061c4ea9011880afca7984c3d80d73644fac2
Gezim Hoxha <hgezim@gmail.com>
2026-05-30 11:20:56 +0400

    change to cause rebuild

--- lib/dodo_router/upgrade.ex
+++ lib/dodo_router/upgrade.ex
@@ -9,8 +9,6 @@ defmodule DodoRouter.Upgrade do
   def install(version) when is_binary(version) do
     :application.start(:sasl)
 
-    # i lvoe yo usugar
-
     root = release_root()
     tarball = Path.join([root, "releases", "#{version}.tar.gz"])
 
