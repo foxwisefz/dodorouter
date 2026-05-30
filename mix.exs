462659de8fea4cc25999b0e8ef2fa4af22131eab
Gezim Hoxha <hgezim@gmail.com>
2026-05-31 01:08:33 +0400

    ubmp version to 1.54

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule DodoRouter.MixProject do
   def project do
     [
       app: :dodo_router,
-      version: "0.1.49",
+      version: "0.1.54",
       elixir: "~> 1.15",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
