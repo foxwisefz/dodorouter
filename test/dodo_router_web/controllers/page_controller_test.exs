ac09c4c7d046205f858bdaa8a6efe6759adde75d
Gezim Hoxha <hgezim@gmail.com>
2026-07-06 10:21:56 +0400

    Land first-time users in the create-router flow after login
    Logging in with zero routers dumped users on the empty 'No routers yet'
    listing. The root path now sends them straight to /routers/new with the
    create modal open.

--- test/dodo_router_web/controllers/page_controller_test.exs
+++ test/dodo_router_web/controllers/page_controller_test.exs
@@ -1,8 +1,15 @@
 defmodule DodoRouterWeb.PageControllerTest do
   use DodoRouterWeb.ConnCase
 
-  test "GET / redirects logged-in users to /routers", %{conn: conn} do
+  test "GET / sends users with no routers straight to the create flow", %{conn: conn} do
     conn = conn |> log_in_user(DodoRouter.AccountsFixtures.user_fixture()) |> get(~p"/")
+    assert redirected_to(conn) == ~p"/routers/new"
+  end
+
+  test "GET / redirects users with routers to /routers", %{conn: conn} do
+    user = DodoRouter.AccountsFixtures.user_fixture()
+    {_router, _key} = DodoRouter.RoutersFixtures.router_fixture(user)
+    conn = conn |> log_in_user(user) |> get(~p"/")
     assert redirected_to(conn) == ~p"/routers"
   end
 
